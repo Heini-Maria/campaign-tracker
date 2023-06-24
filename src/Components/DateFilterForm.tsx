22f8d011a302d89c0b0f7acadd01e9f8504e1740
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2023-06-24 06:50:21 +0200

    added datePickers

--- src/Components/DateFilterForm.tsx
+++ src/Components/DateFilterForm.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-date-picker";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
 import { FaTimes } from "react-icons/fa";
 import { DateFilterFormProps } from "../Utils/types";
 
 function DateFilterForm({ toggleFilterForm }: DateFilterFormProps) {
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
   return (
     <div className="dateFilterForm">
+       {console.log(startDate)}
       <button type="button" onClick={toggleFilterForm}>
         <FaTimes />
       </button>
       <form>
-        <input />
-        <input />
+        <label>Start date:</label>
+        <DatePicker value={startDate} onChange={setStartDate} />
+        <label>End date:</label>
+        <DatePicker
+          value={endDate}
+          onChange={setEndDate}
+          minDate={startDate}
+          disabled={startDate === undefined}
+        />
         <button type="submit">Filter</button>
       </form>
     </div>
